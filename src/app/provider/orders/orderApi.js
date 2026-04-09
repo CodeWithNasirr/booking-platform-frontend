1a2c36f8bbd2fd7eaa773313d69884c0fb6ba43e
Sk Nasir <skofficial665@gmail.com>
2026-04-09 11:19:13 +0530

    production test

--- src/app/provider/orders/orderApi.js
+++ src/app/provider/orders/orderApi.js
@@ -25,7 +25,6 @@ export const authFetch = async (path, tenantId, options = {}) => {
     "X-Tenant": tenantId,
     ...(token && { Authorization: `Bearer ${token}` }),
     ...(options.headers || {}),
-    credentials: "include",
   };
   
 
@@ -36,6 +35,8 @@ export const authFetch = async (path, tenantId, options = {}) => {
   const res = await fetch(`${API_BASE}${path}`, {
     ...options,
     headers,
+    credentials: "include",
+
   });
 
   let data = null;
