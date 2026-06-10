4293398efe1557684472ad4f50cc22ffffa4cc2d
Sk Nasir <skofficial665@gmail.com>
2026-06-10 23:02:13 +0530

    onboarding update

--- src/lib/cookieConfig.js
+++ src/lib/cookieConfig.js
@@ -1,9 +1,12 @@
-export const COOKIE_OPTIONS = {
-  domain:
-    process.env.NODE_ENV === "development"
-      ? ".lvh.me"
-      : ".yourplatform.com",
+// lib/cookieConfig.js
 
+const isProd = process.env.NODE_ENV === "production";
+
+export const COOKIE_OPTIONS = {
   path: "/",
   sameSite: "lax",
+  secure: isProd,
+  ...(isProd && {
+    domain: ".neoleap.ai",
+  }),
 };
